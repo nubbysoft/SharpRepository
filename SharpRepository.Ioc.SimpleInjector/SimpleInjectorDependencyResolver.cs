54b4e440dd03f8af819229ab339a4fb2f0794a07
Sébastien BIAUDET <sebastien.biaudet@gmail.com>
2013-07-18 16:27:19 +0200

    Added SimpleInjector IOC support

---
+++ SharpRepository.Ioc.SimpleInjector/SimpleInjectorDependencyResolver.cs
@@ -0,0 +1,31 @@
+﻿using SharpRepository.Repository.Ioc;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpRepository.Ioc.SimpleInjector
+{
+    public class SimpleInjectorDependencyResolver : BaseRepositoryDependencyResolver
+    {
+        private readonly Container _container;
+
+        public SimpleInjectorDependencyResolver(Container container)
+        {
+            _container = container;
+        }
+
+        protected override T ResolveInstance<T>()
+        {
+           return (T)ResolveInstance(typeof(T));
+        }
+
+        protected override object ResolveInstance(Type type)
+        {
+            return _container.GetInstance(type); ;
+        }
+    }
+
+}
